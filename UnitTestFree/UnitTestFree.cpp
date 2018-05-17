3527c643b673c2e6fa002874a3cf9f45d04a32c0
SpingYan <SpingYan@users.noreply.github.com>
2018-05-17 15:50:55 +0800

    Add files via upload

---
+++ UnitTestFree/UnitTestFree.cpp
@@ -0,0 +1,16 @@
+// UnitTestFree.cpp : 定義主控台應用程式的進入點。
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	char c1='k', c2='K', c3; 
+	c3=c2-c1+'a'+7;
+	cout<<c3<<endl;
+	return 0; 	
+}
+
